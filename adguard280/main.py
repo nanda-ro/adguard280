8be5f077ad5a547751961b3182fef8247bc31983
yuuki <you@nanda.ro>
2022-05-17 09:36:24 +0900

    fix url format and add comment

--- adguard280/main.py
+++ adguard280/main.py
@@ -3,15 +3,15 @@ from fastapi.responses import PlainTextResponse
 import requests
 import datetime
 
-URL_PREFIX = "https://280blocker.net/files/280blocker_domain_ag_"
-URL_SUFFIX = ".txt"
+URL_FORMAT = "https://280blocker.net/files/280blocker_domain_ag_{}{:0>2}.txt"
 
 app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
 def index():
     today = datetime.date.today()
-    url = "{}{}{:0>2}{}".format(URL_PREFIX, today.year, today.month, URL_SUFFIX)
+    url = URL_FORMAT.format(today.year, today.month) # URLを生成
     response = requests.get(url)
-    response.encoding = "UTF-8"
-    return response.text
+    response.encoding = "UTF-8" # テキストエンコーディングが正しく返却されないので指定
+
+    return response.text # 取得したリストをそのままテキスト形式で返却
